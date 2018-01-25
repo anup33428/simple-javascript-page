72a0878d634595fa2027725bba73ab8398807109
Maria Rice <mcrice@shockers.wichita.edu>
2018-01-25 08:16:36 -0600

    Modify html form and add new function for submit
    Form submission calls validation function to check if fields are
    empty. If they are, an error pop-up appears to alert the user to
    fill in those fields. Otherwise, a success pop-up displays the
    full name entered.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function myName() {
 	return "Patrick"
 }
+
+function validateForm() {
+	var firstname = document.forms["myForm"]["firstname"].value;
+	var lastname = document.forms["myForm"]["lastname"].value;
+	if ((firstname == "") || (lastname == "")) {
+		alert("Both fields must be filled out");
+		return false;
+	}
+	else {
+		alert("Success! You entered '" + firstname + " " + lastname + "'");
+		return true;
+	}
+}
